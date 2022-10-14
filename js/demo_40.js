2a28dbaeca87929b8b53032232ecc7152c541f28
sean <sean20020313@gmail.com>
2022-10-14 17:35:03 +0800

    w06 upload

---
+++ js/demo_40.js
@@ -0,0 +1,20 @@
+function change(index){
+
+const p =document.querySelector(".show-classdemo")
+   console.log('p',p);
+   switch(index){
+    case 1:
+    p.innerHTML=`<iframe src="./demo/w01_40/index.html" width="100%" height="100%"/>`
+        break;
+        case 2:
+        p.innerHTML=`<iframe src="./demo/w02_40/card_40.html" width="100%" height="100%"/>`
+            break;
+            case 3:
+                p.innerHTML=`<iframe src="./demo/w03_40/blog_40.html" width="100%" height="100%"/>`
+                break;
+                case 4:
+                    p.innerHTML=`<iframe src="./demo/w06_40/blog_40.html" width="100%" height="100%"/>`
+                    break;
+   }
+
+}
